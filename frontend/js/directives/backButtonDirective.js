600702b4cc85660eceaaceb76927bd2012f0d583
Sergey Ladutko <verus774@mail.ru>
2017-01-05 12:08:57 +0300

    Frontend: backButton directive added

---
+++ frontend/js/directives/backButtonDirective.js
@@ -0,0 +1,19 @@
+angular
+    .module('pollsApp')
+    .directive('backButton', backButton);
+
+backButton.$inject = ['$window'];
+
+function backButton($window) {
+    function link(scope, element, attrs) {
+        element.bind('click', function () {
+            $window.history.back();
+        });
+    }
+
+    return {
+        link: link,
+        restrict: 'A'
+    };
+
+}
