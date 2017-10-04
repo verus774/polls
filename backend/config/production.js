c113b4a646f4b5284c11d71783f513b227943bea
Sergey Ladutko <verus774@mail.ru>
2017-10-04 22:04:25 +0300

    backend: fix prod config

--- backend/config/production.js
+++ backend/config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 80,
-    dbUrl: process.env.DB_URL_PROD,
+    dbUrl: process.env.DB_URL,
     secretKey: process.env.SECRET_KEY,
     tokenExpiresIn: '2d',
     serveStatic: '../frontend/build'
